67a5d244e6b5ffdf3fcc37cb7bf6fd477eb7e6f6
Gimei Alex <gimeialez@gmail.com>
2023-12-28 14:31:46 +0300

    started the build up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://api.api-ninjas.com/v1/celebrity?name=${searchTerm}`,
+          {
+            headers: {
+              'X-Api-Key' : 'INSERT API KEY'
+            }
+          }
+        );
+        const data = await res.json();
+        searchResults(data);
+      }catch (error) {
+        setError(error);
+      }
+      setIsLoading(false)
+    }
+    if(searchTerm.length > 0){
+      fetchData();
+    }
+  }, [searchTerm])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label> Search for a celebrity networth:</label>
+        <input 
+        type='text'
+        id='search'
+        value={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+        ></input>
+      </form>
+      {isLoading ? (
+        <p>One moment please...</p>
+      ) : (
+        searchResults.map(result => {
+          <div key='result.id'>
+            <h2>{result.name}</h2>
+            <h3>Net woth: {result.net_worth}</h3>
+          </div>
+        })
+      )}
     </div>
   );
 }
